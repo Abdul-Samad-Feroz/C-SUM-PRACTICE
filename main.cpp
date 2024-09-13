a4dbba629bdab8d3e88484dcf80c87a96335f7c8
Abdul Samad <abdulsamadferoz786@gmail.com>
2024-09-13 22:22:14 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 30;
+    int num3 = 50;
+    int avg = (num1 + num2 + num3)/3;
+    cout<< avg << endl;
+    return 0;
+}
